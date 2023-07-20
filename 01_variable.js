4bdbe1b9eea2d148150aa65708d1e59300e278b4
Kolekarpriyanka <nikitakolekar12345@gmail.com>
2023-07-20 15:05:29 +0530

    datatypes
    datatypes

--- 01_variable.js
+++ 01_variable.js
@@ -8,3 +8,13 @@ console.log(city);
 
 var pin;//variable Declaration
 console.log(pin);
+
+console.log('================');
+var salary= null;
+console.log( typeof salary);
+salary=40000.50;//
+ var typeOfSalary  = typeof salary;
+console.log('salary=>',salary, 'typeof salary=>' , typeOfSalary );
+salary="forty thousand";
+var salaryTypeOf=typeof salary;
+console.log( 'salary=>',salary,'typeof=>',salaryTypeOf);
