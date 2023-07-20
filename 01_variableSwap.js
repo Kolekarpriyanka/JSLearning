52ba343cac2d7d42acdea5d690ae6b88561ee9df
Kolekarpriyanka <nikitakolekar12345@gmail.com>
2023-07-20 15:44:27 +0530

    variableSwap
    variableSwap

---
+++ 01_variableSwap.js
@@ -0,0 +1,8 @@
+var num1=100;//200
+var num2=200;//100
+console.log( 'Before Swap','num1-',num1,'num2-',num2);
+var temp =num1;
+num1=num2;
+num2=temp;
+console.log(temp,num1,num2);
+console.log('After Swap','num1-' ,num1,'num2-',num2);
