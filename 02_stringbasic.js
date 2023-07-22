2f1dbdd16b634889b175be4131c1b3fe6279fa13
Kolekarpriyanka <nikitakolekar12345@gmail.com>
2023-07-22 18:01:15 +0530

    javaScript string basics
    javaScript string basics

---
+++ 02_stringbasic.js
@@ -0,0 +1,16 @@
+function stringBasic() {
+    console.log(`               **Assignment-1**                                             `);
+    console.log(`-------------------_Step-1__---------------------------------------------------`);//step 1
+    var dream ="TCS and Microsoft";//initialization and declaration)
+    console.log(`My Dream Company is ${dream}`);//log on in console
+}
+stringBasic();//calling Finction
+console.log(`-------------------_Step-2__-------------------------------------------------------`);//step 2
+var hobby1="DesignUsingCoding";
+var hobby2="LearningNewTechnicalHacks";
+var hobby3="SolvingRubikCube";
+console.log(`My Hobbies are :  ${hobby1}, ${hobby2}, ${hobby3}`);
+console.log(`-------------------_Step-3__-------------------------------------------------------`);//step 3
+var sumofhobby=hobby1.length+hobby2.length+hobby3.length;//doing addition meanwhile calculate the length as well
+console.log(`Sum Of Total Number of Characters is :  ${sumofhobby}`);
+console.log(`*******************************`);
