e873c3991cf92750f2049b67c885f2cd253fc429
Kolekarpriyanka <nikitakolekar12345@gmail.com>
2023-08-01 15:16:45 +0530

    Assignment Using for_loop and while_loop
    Assignment Using for_loop and while_loop

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,34 @@
+console.log(`...........Assignment using for loop..............`);
+console.log(`..........Number from 5 to 15 by incrementing 1 .................`);
+for (let index = 5; index <=15;  index=index+5) {
+    console.log(index);
+    
+}
+console.log(`........................numbers from 50 to 40 by decrementing by 1................`);
+for (let index = 50; index >=40;  index=index-1) {
+    console.log(index);
+    
+}
+console.log(`.....................frist 15 odd number...........................................`);
+for (let index = 1; index <=15;  index=index+2) {
+    console.log(index);
+    
+}
+console.log(`...... ..............5 table..........................`);
+for (let index = 5; index <=50;  index=index+5) {
+    console.log(index);
+    
+}
+console.log(` ...............10 table..............`);
+for (let index = 10; index <=100;  index=index+10) {
+    console.log(index);
+    
+}
+console.log(`.................10 table in reverse order.........`);
+var i=100
+for (let index = 100; index >=10; index= index-10) {
+    console.log(index);
+    i=i-10
+    
+}
+
---
+++ 06_whileloop Ass02.js
@@ -0,0 +1,47 @@
+console.log(`.............Assignment using while loop.................`)
+console.log(`.......... numbers from 5to 15....................`);
+ var i=5;
+
+while (i<=15) {
+ console.log(i);  
+ i++;
+}
+console.log(`............Number from 50 to40 decrement by 1..................`);
+var i=50
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+console.log(`....................frist 15 odd number.........`);
+var i=1
+while (i<=15) {
+    console.log(i);
+    i= i+2;
+}
+console.log(`..............................Find frist 10 Even number...........................................`);
+var i=0
+while    (i<=10) {
+    console.log(i);
+    i=i+2;
+}
+console.log(`................table of 5...................`);
+ var i=5
+ while (i<=50) {
+    console.log(i);
+    i=i+5;
+ }
+ console.log(`-------.table of 10....................`);
+ var i=10;
+ while (i<=100) {
+    console.log(i);
+    i=i+10;
+ }
+
+console.log(`.............reverse table of 10...........`);
+
+ var i=100;
+ while (i>10) {
+    console.log(i);
+    i=i-10
+ }
+ console.log(`.......................................`);
